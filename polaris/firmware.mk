bbe98b5ea5381e95a9ceafd440e6209512aa2787
buzzcola3 <buzzcola3@gmail.com>
2021-11-12 21:27:36 +0100

    add polaris

---
+++ polaris/firmware.mk
@@ -0,0 +1,18 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi-firmware/polaris/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi-firmware/polaris/aop.img:install/firmware-update/aop.img \
+    vendor/xiaomi-firmware/polaris/bluetooth.img:install/firmware-update/bluetooth.img \
+    vendor/xiaomi-firmware/polaris/cmnlib.img:install/firmware-update/cmnlib.img \
+    vendor/xiaomi-firmware/polaris/cmnlib64.img:install/firmware-update/cmnlib64.img \
+    vendor/xiaomi-firmware/polaris/devcfg.img:install/firmware-update/devcfg.img \
+    vendor/xiaomi-firmware/polaris/dsp.img:install/firmware-update/dsp.img \
+    vendor/xiaomi-firmware/polaris/hyp.img:install/firmware-update/hyp.img \
+    vendor/xiaomi-firmware/polaris/keymaster.img:install/firmware-update/keymaster.img \
+    vendor/xiaomi-firmware/polaris/logo.img:install/firmware-update/logo.img \
+    vendor/xiaomi-firmware/polaris/modem.img:install/firmware-update/modem.img \
+    vendor/xiaomi-firmware/polaris/qupfw.img:install/firmware-update/qupfw.img \
+    vendor/xiaomi-firmware/polaris/storsec.img:install/firmware-update/storsec.img \
+    vendor/xiaomi-firmware/polaris/tz.img:install/firmware-update/tz.img \
+    vendor/xiaomi-firmware/polaris/xbl_config.img:install/firmware-update/xbl_config.img \
+    vendor/xiaomi-firmware/polaris/xbl.img:install/firmware-update/xbl.img
